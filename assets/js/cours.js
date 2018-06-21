695a943b83f47b745fe6376b247d3b091d923b3e
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-21 15:06:21 +0200

    mise à jour après correction

--- assets/js/cours.js
+++ assets/js/cours.js
@@ -1,2 +1,3 @@
-var prenom = prompt("Entrez votre prénom: ");
-console.log("Bonjour " + prenom);
+/*Déclaration d'une variable pour mettre son nom*/
+var firstname = prompt('Entrez votre prénom: ');
+alert('Bonjour ' + firstname);
